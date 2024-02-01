8c2dd41cca8bd3773ea28823f7904d461f9a7160
jororlando-81 <jorgeorlando81@hotmail.com>
2024-02-01 20:11:19 -0300

    Project Created

---
+++ src/main/java/com/project/personregistration/PersonregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.project.personregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonregistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/personregistration/PersonregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.personregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
